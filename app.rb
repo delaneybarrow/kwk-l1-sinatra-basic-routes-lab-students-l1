490d0183ee8127621279eb197e0dfab33ccc0def
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-12 22:40:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do 
+    "My name is Delaney"
+  end
+  
+  get '/hometown' do
+    "My hometown is Tucson"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is Sunflower"
+  end
 end
